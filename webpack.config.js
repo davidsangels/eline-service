d4b8c7754b1a5660f5c59b177d42baf97fa90340
Elinex <eline.ax@gmail.com>
2019-07-05 12:24:07 -0700

    install dependencies

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  watch: true,
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+};
