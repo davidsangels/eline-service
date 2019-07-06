72ba0f2f56f24d69c7827cd7a56b43243e08028d
Elinex <eline.ax@gmail.com>
2019-07-05 21:23:45 -0700

    create mock data with sequelize

--- database/seed.js
+++ database/seed.js
@@ -4,7 +4,8 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('infoPlace', 'root', '', {
   host: 'localhost',
-  dialect: 'mysql'
+  dialect: 'mysql',
+  timestamps: false
 });
 
 sequelize
@@ -15,3 +16,62 @@ sequelize
   .catch(err => {
     console.error('Unable to connect to the database:', err);
   });
+
+class Review extends Sequelize.Model {};
+Review.init(
+  // attributes
+  {
+    idPlace: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    username: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    idUser: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+    text: {
+      type: Sequelize.TEXT,
+      allowNull: false
+    },
+    avatarUrl: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  },
+  // options
+  {sequelize}
+);
+
+var createPlaceIds = function(){
+  var placeIds = [];
+  for (var i = 0; i < 5; i++){
+    placeIds.push(faker.random.number())
+  }
+  return placeIds;
+}
+
+Review.sync({ force: true })
+  .then(() => {
+    var placeIds = createPlaceIds();
+    for (var i = 0; i < placeIds.length; i++){
+      var reviewsNum = Math.floor(Math.random() * Math.floor(60));
+      for (var j = 0; j < reviewsNum; j++){
+        Review.create({
+          idPlace: placeIds[i],
+          username: faker.name.findName(),
+          idUser: faker.random.number(),
+          createdAt: faker.date.past(),
+          text: faker.lorem.text(),
+          avatarUrl: faker.image.avatar()
+        });
+      }
+    }
+});
--- database/test.js
+++
@@ -1,3 +0,0 @@
-const faker = require('faker')
-
-console.log(typeof faker.date.past())
