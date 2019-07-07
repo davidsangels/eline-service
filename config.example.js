85ed01a19620d0fe29fecfd385049c367f7df12d
Elinex <eline.ax@gmail.com>
2019-07-06 20:09:08 -0700

    install express server

---
+++ config.example.js
@@ -0,0 +1,16 @@
+// MySql Connection
+const mysqlData = {
+  user: 'FILL_ME',
+  password: 'FILL_ME'
+};
+
+// Sequelize
+const sequelizeData = {
+  user: 'FILL_ME',
+  password: 'FILL_ME'
+};
+
+module.exports = {
+  mysqlData,
+  sequelizeData
+}
