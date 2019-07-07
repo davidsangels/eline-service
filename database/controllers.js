5348d0d9165226fccc656bdf9dccae4013adedd5
Elinex <eline.ax@gmail.com>
2019-07-06 22:08:20 -0700

    get reviews by placeId

--- database/controllers.js
+++ database/controllers.js
@@ -1,11 +1,14 @@
 const { Review } = require('./models.js')
 
-const getReviewsById = (id) => {
-  // Review.findAll({
-  //   where: {
-  //     idPlace: id
-  //   }
-  // })
+const getReviewsById = (id, callback) => {
+  console.log(id)
+  Review.findAll({
+    where: {
+      idPlace: id
+    }
+  })
+  .then(data => {callback(null, data)})
+  .catch(err => {callback(err)})
 }
 
 module.exports = {
--- server/index.js
+++ server/index.js
@@ -8,7 +8,14 @@ app.use(express.static('public'))
 // GET /api/reviews/:placeId
 // Get all the reviews for a specific place.
 app.get('/api/reviews/:placeId', (req, res) => {
-
+  getReviewsById(req.params.placeId, (err, reviews) => {
+    if(err){
+      console.log(err)
+    }
+    else {
+      res.send(reviews)
+    }
+  })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
