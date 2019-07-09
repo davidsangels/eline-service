df7fc094d76c0b2ee7316bf786fe9c804748e353
Elinex <eline.ax@gmail.com>
2019-07-09 16:57:59 -0700

    search component skeleton

--- client/components/App.js
+++ client/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Search from './Search';
 
 class App extends React.Component {
   constructor(props){
@@ -8,7 +9,11 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        Hello!
+        <header>
+          <h4>357 Reviews</h4>
+          <div>stars</div>
+          <Search />
+        </header>
       </div>
     )
   }
---
+++ client/components/Search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Search = () => {
+  return (
+    <input
+      type="text"
+      name="review-search-box"
+      placeholder="Search reviews"
+      // TODO: value will be received from App component
+      // value="..."
+    />
+  )
+};
+
+export default Search;
