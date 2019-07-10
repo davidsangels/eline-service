82bc0544e449bb718d34defe23e80fb47a555984
Elinex <eline.ax@gmail.com>
2019-07-09 20:07:46 -0700

    fix avatar size

--- client/components/Review.js
+++ client/components/Review.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Review = ({review}) => {
   return (
     <div>
-      <img src={review.avatarUrl} />
+      <img src={review.avatarUrl} height="50" width="50" />
       <span>{review.username}</span>
       <span>{review.createdAt}</span>
       <p>{review.text}</p>
