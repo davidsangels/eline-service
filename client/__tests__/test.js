ed8378dc4cc0751a0fc6147e68b8604a525a593a
Elinex <eline.ax@gmail.com>
2019-07-10 17:54:48 -0700

    create more tests to App component

--- client/__tests__/test.js
+++ client/__tests__/test.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import App from '../App.js';
-import renderer from 'react-test-renderer';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
 
-describe('<App />', () => {
-  it('renders correctly text component', () => {
-    const TextInputComponent = renderer.create(<App />).toJSON();
-    expect(TextInputComponent).toMatchSnapshot();
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../components/App';
+
+describe('App', () => {
+  it('should render correctly in "debug" mode', () => {
+    const component = shallow(<App debug/>);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should render correctly with no props', () => {
+    const component = shallow(<App/>);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should render banner text correctly with given strings', () => {
+    const strings = ['one', 'two'];
+    const component = shallow(<App list={strings} />);
+    expect(component).toMatchSnapshot();
   });
 });
--- client/components/App.js
+++ client/components/App.js
@@ -17,8 +17,6 @@ class App extends React.Component {
   }
 
   render(){
-
-
     return (
       <div>
         <div style={{display: 'flex', alignItems: 'center'}}>
