2512d386f31df2699835ef24ce5542d2c4ed2c67
Elinex <eline.ax@gmail.com>
2019-07-11 14:42:20 -0700

    create new fields for Review model

--- database/models.js
+++ database/models.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize')
 const sequelize = require('./index.js')
 
 const Review = Sequelize.Model;
+// const AverageRating = Sequelize.Model;
 
 Review.init(
   // attributes:
@@ -29,7 +30,35 @@ Review.init(
     avatarUrl: {
       type: Sequelize.STRING,
       allowNull: false
-    }
+    },
+    accuracy_rating: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    communication_rating: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    cleanliness_rating: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    location_rating: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    checkin_rating: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    value_rating: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    overall_rating: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
   },
   // options:
   {
@@ -39,4 +68,51 @@ Review.init(
   }
 );
 
-module.exports = Review
+// AverageRating.init(
+//   // attributes:
+//   {
+//     idPlace: {
+//       type: Sequelize.INTEGER,
+//       allowNull: false
+//     },
+//     accuracy_avg: {
+//       type: Sequelize.INTEGER,
+//       allowNull: false
+//     },
+//     communication_avg: {
+//       type: Sequelize.INTEGER,
+//       allowNull: false
+//     },
+//     cleanliness_avg: {
+//       type: Sequelize.INTEGER,
+//       allowNull: false
+//     },
+//     location_avg: {
+//       type: Sequelize.INTEGER,
+//       allowNull: false
+//     },
+//     checkin_avg: {
+//       type: Sequelize.INTEGER,
+//       allowNull: false
+//     },
+//     value_avg: {
+//       type: Sequelize.INTEGER,
+//       allowNull: false
+//     },
+//     overall_avg: {
+//       type: Sequelize.INTEGER,
+//       allowNull: false
+//     },
+//   },
+//   // options:
+//   {
+//     sequelize,
+//     modelName: 'averageRatings',
+//     timestamps: false
+//   }
+// );
+
+module.exports = {
+  Review,
+  // AverageRating
+}
--- database/seed.js
+++ database/seed.js
@@ -1,4 +1,5 @@
-const Review = require('./models.js')
+const { Review } = require('./models.js')
+// const { AverageRating } = require('./models.js')
 const faker = require('faker')
 
 const createPlaceIds = function(){
@@ -9,9 +10,10 @@ const createPlaceIds = function(){
   return placeIds
 }
 
+const placeIds = createPlaceIds()
+
 Review.sync({ force: true })
 .then(() => {
-  var placeIds = createPlaceIds()
   for (var i = 0; i < placeIds.length; i++){
     var reviewsNum = Math.floor(Math.random() * Math.floor(60))
     for (var j = 0; j < reviewsNum; j++){
@@ -21,8 +23,20 @@ Review.sync({ force: true })
         idUser: faker.random.number(),
         createdAt: faker.date.past(),
         text: faker.lorem.text(),
-        avatarUrl: faker.image.avatar()
+        avatarUrl: faker.image.avatar(),
+        accuracy_rating: faker.random.number(5),
+        communication_rating: faker.random.number(5),
+        cleanliness_rating: faker.random.number(5),
+        location_rating: faker.random.number(5),
+        checkin_rating: faker.random.number(5),
+        value_rating: faker.random.number(5),
+        overall_rating: faker.random.number(5)
       })
     }
   }
 })
+
+// AverageRating.sync({ force: true })
+// .then(() => {
+
+// })
