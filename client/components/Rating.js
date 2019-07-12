3e3feeffd2db31b945d0bdbd828aaa49478898ce
Elinex <eline.ax@gmail.com>
2019-07-11 20:35:30 -0700

    fix color to starNotFilled

--- client/components/Rating.js
+++ client/components/Rating.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
+// needs to refine this component
+// the ratings must be with two decimal houses
+// the stars should render a different background if value is not an integer (or decimal != 00)
 const Rating = ({rating}) => {
   const starsFilled = [];
   for (var i = 0; i < rating; i++){
-    starsFilled.push(<ion-icon name="star"></ion-icon>)
+    starsFilled.push(
+      <ion-icon name="star" key={i}></ion-icon>
+    )
   }
   const starsNotFilled = [];
   for (var i = 0; i < 5 - rating; i++){
-    starsNotFilled.push(<ion-icon name="star" style={{backgroundColor: 'pink'}}></ion-icon>)
+    starsNotFilled.push(
+      <ion-icon name="star" key={i} style={{color: 'pink'}}></ion-icon>
+    )
   }
   return (
     <div>
