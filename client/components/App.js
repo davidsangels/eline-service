6ab5e665a9139c2c7c074397304ae82372affb29
Elinex <eline.ax@gmail.com>
2019-07-11 19:59:13 -0700

    refactor App component to receive all idPlaces

--- client/components/App.js
+++ client/components/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import $ from 'jquery';
+
 import Search from './Search';
 import Rating from './Rating';
 import Attributes from './Attributes';
@@ -12,25 +14,57 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeBtn: 1
+      activeBtn: 1,
+      currentPlace: null,
+      places: []
     };
   }
 
+  componentDidMount(){
+    // logs all idPlaces
+    $.ajax({
+      type: 'GET',
+      url: '/api/places'
+    })
+    .done(places => {
+      const idPlaces = places.reduce((acc, cur) => {
+        return acc.concat(cur.idPlace)
+      }, []);
+
+      console.log(
+        "%c These are the idPlaces available: ",
+        "color: blue; font-size: 16px",
+        idPlaces,
+      );
+
+      return this.setState({
+        currentPlace: idPlaces[0],
+        places: idPlaces
+      })
+    })
+  }
+
   render() {
+    const { currentPlace, places } = this.state;
+
     return (
       <div>
-        <div style={{display: 'flex', alignItems: 'center'}}>
-          <h4>357 Reviews</h4>
-          <Rating />
-          <Search />
-        </div>
-        <hr />
-        <Attributes />
-        <Reviews reviews={mockData} />
-        <Pagination
-          numBtn={numBtn}
-          activeBtn={this.state.activeBtn}
-        />
+        {currentPlace !== 'null' && (
+          <div>
+            <div style={{display: 'flex', alignItems: 'center'}}>
+              <h4>357 Reviews</h4>
+              <Rating />
+              <Search />
+            </div>
+            <hr />
+            <Attributes />
+            <Reviews reviews={mockData} />
+            <Pagination
+              numBtn={numBtn}
+              activeBtn={this.state.activeBtn}
+            />
+          </div>
+        )}
       </div>
     );
   }
--- client/index.js
+++ client/index.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
 
 import App from './components/App';
-
-// get all idPlaces
-$.ajax({
-  type: 'GET',
-  url: '/api/places'
-})
-.done(places => {
-  console.log(
-    "%c These are the idPlaces available: ",
-    "color: blue; font-size: 16px",
-    places,
-  )})
 
 ReactDOM.render(
   <App/>,
