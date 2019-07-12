d17c40a06207be8b94bec050ac309a6d82d73832
Elinex <eline.ax@gmail.com>
2019-07-11 21:53:12 -0700

    insert type of star to be rendered

--- client/components/Rating.js
+++ client/components/Rating.js
@@ -1,27 +1,69 @@
 import React from 'react';
 
-// needs to refine this component
-// the ratings must be with two decimal houses
-// the stars should render a different background if value is not an integer (or decimal != 00)
+const Star = (type) => (
+  <ion-icon name="star" type={type}></ion-icon>
+);
+
 const Rating = ({rating}) => {
-  const starsFilled = [];
-  for (var i = 0; i < rating; i++){
-    starsFilled.push(
-      <ion-icon name="star" key={i}></ion-icon>
-    )
+  const typeStars = (value) => {
+    const result = {};
+
+    if(value % 1 === 0){
+      result.notFilled = 5 - value;
+      result.filled = 5 - result.notFilled;
+      result.partialFilled = 0;
+    } else {
+      result.filled = Math.floor(value);
+      result.partialFilled = Number((value % 1).toFixed(2));
+      result.notFilled = 5 - result.filled - Math.ceil(result.partialFilled);
+    }
+    return result;
   }
-  const starsNotFilled = [];
-  for (var i = 0; i < 5 - rating; i++){
-    starsNotFilled.push(
-      <ion-icon name="star" key={i} style={{color: 'pink'}}></ion-icon>
-    )
+
+  const buildStars = () => {
+    const stars = []
+    const types = typeStars(rating);
+    for (var key in types){
+      const type = key;
+      const numStars = types[key]
+      for (let i = 0; i < numStars; i++){
+        stars.push(Star(type))
+      }
+    }
+    return stars;
   }
+
+  // buildStars()
+
   return (
     <div>
-      {starsFilled}
-      {starsNotFilled}
+      {buildStars()}
     </div>
-  );
+
+  )
+
+
+
 };
 
 export default Rating;
+
+// const starsFilled = [];
+//   for (var i = 0; i < rating; i++){
+//     starsFilled.push(
+//       <ion-icon name="star" key={i}></ion-icon>
+//     )
+//   }
+//   const starsNotFilled = [];
+//   for (var i = 0; i < 5 - rating; i++){
+//     starsNotFilled.push(
+//       <ion-icon name="star" key={i} style={{color: 'pink'}}></ion-icon>
+//     )
+//   }
+
+//   return (
+//     <div>
+//       {starsFilled}
+//       {starsNotFilled}
+//     </div>
+//   );
