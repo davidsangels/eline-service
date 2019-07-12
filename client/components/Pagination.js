5839d85028717bd0c63546f367f43d4a6915308e
Elinex <eline.ax@gmail.com>
2019-07-12 16:21:37 -0700

    render buttons with pagination function

--- client/components/Pagination.js
+++ client/components/Pagination.js
@@ -6,13 +6,21 @@ const Pagination = ({
   currentPage,
   numBtns,
 }) => {
+  const buttons = pagination(currentPage, numBtns)
   return (
     <div>
-      {pagination(currentPage, numBtns)}
+      {buttons.map(btn => (
+        <button
+          type='button'
+          value={btn}
+          key={btn}
+          // onClick={(e) => changePage(e.target.value)}
+        >{btn}</button>
+      ))}
       <button
         type='button'
         value='next'
-        onClick={(e) => changePage(e.target.value)}
+        // onClick={(e) => changePage(e.target.value)}
       >Next</button>
     </div>
   );
