40501a07255625b87e2ec548e5209cab89bdc5de
Elinex <eline.ax@gmail.com>
2019-07-11 17:56:44 -0700

    create get request to fetch ratings by idPlace

--- database/controllers.js
+++ database/controllers.js
@@ -1,4 +1,4 @@
-const Review = require('./models.js')
+const { Review, AverageRating } = require('./models.js')
 
 const getReviewsById = (id, callback) => {
   Review.findAll({
@@ -10,6 +10,17 @@ const getReviewsById = (id, callback) => {
   .catch(err => {callback(err)})
 }
 
+const getRatingsById = (id, callback) => {
+  AverageRating.findAll({
+    where: {
+      idPlace: id
+    }
+  })
+  .then(data => {callback(null, data)})
+  .catch(err => {callback(err)})
+}
+
 module.exports = {
-  getReviewsById
+  getReviewsById,
+  getRatingsById
 }
--- server/index.js
+++ server/index.js
@@ -1,21 +1,35 @@
 const express = require('express')
 const app = express()
 const port = 3000
-// const { getReviewsById } = require('../database/controllers.js')
+const {
+  getReviewsById,
+  getRatingsById
+} = require('../database/controllers.js')
 
 app.use(express.static('public'))
 
-// GET /api/reviews/:placeId
 // Get all the reviews for a specific place.
-// app.get('/api/reviews/:idPlace', (req, res) => {
-//   getReviewsById(req.params.idPlace, (err, reviews) => {
-//     if(err){
-//       console.log(err)
-//     }
-//     else {
-//       res.send(reviews)
-//     }
-//   })
-// })
+app.get('/api/reviews/:idPlace', (req, res) => {
+  getReviewsById(req.params.idPlace, (err, reviews) => {
+    if(err){
+      console.log(err)
+    }
+    else {
+      res.send(reviews)
+    }
+  })
+})
+
+// Get all the ratings for a specific place.
+app.get('/api/ratings/:idPlace', (req, res) => {
+  getRatingsById(req.params.idPlace, (err, ratings) => {
+    if(err){
+      console.log(err)
+    }
+    else {
+      res.send(ratings)
+    }
+  })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
