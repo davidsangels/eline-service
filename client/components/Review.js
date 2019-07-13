687580ef34ec3691e8ac65dfe27171a4367639df
Elinex <eline.ax@gmail.com>
2019-07-13 10:05:34 -0700

    change avatar border radius

--- client/components/Review.js
+++ client/components/Review.js
@@ -3,13 +3,39 @@ import React from 'react';
 const Review = ({review}) => {
   return (
     <div>
-      <img src={review.avatarUrl} height="50" width="50" />
-      <span>{review.username}</span>
-      <span>{review.createdAt}</span>
+      <div style={styles.user}>
+        <img
+          src={review.avatarUrl}
+          height="50"
+          width="50"
+          style={styles.img}
+        />
+        <div style={styles.userInfo}>
+          <div>{review.username}</div>
+          <div>{review.createdAt}</div>
+        </div>
+      </div>
       <p>{review.text}</p>
       <hr />
     </div>
   );
 };
+
+const styles = {
+  user: {
+    height: '48px',
+    width: '48px',
+    display: 'flex',
+  },
+  userInfo: {
+    marginLeft: '16px'
+  },
+  img: {
+    backgroundColor: '#D8D8D8',
+    borderRadius: '50%',
+    borderWidth: '2px',
+    borderColor: '#ffffff'
+  }
+}
 
 export default Review;
