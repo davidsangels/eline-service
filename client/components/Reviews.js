b39d2cd139bc154df6de3a7cb94069141ef814a8
Elinex <eline.ax@gmail.com>
2019-07-13 09:52:40 -0700

    marginTop to Reviews divMain

--- client/components/Reviews.js
+++ client/components/Reviews.js
@@ -3,12 +3,18 @@ import Review from './Review';
 
 const Reviews = ({reviews}) => {
   return (
-    <div>
+    <div style={styles.divMain}>
       {reviews.map(review => (
         <Review review={review} key={review.id} />
       ))}
     </div>
   );
 };
+
+const styles = {
+  divMain: {
+    marginTop: '16px'
+  }
+}
 
 export default Reviews;
