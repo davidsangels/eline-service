6711c418400c8d6a0076b93f5d357657d53bf376
Elinex <eline.ax@gmail.com>
2019-07-13 11:13:21 -0700

    add styles to pagination buttons

--- client/components/Pagination.js
+++ client/components/Pagination.js
@@ -15,12 +15,15 @@ const Pagination = ({
           type='button'
           value='before'
           onClick={(e) => changePage(e.target.value)}
-        >Before</button>
+          style={styles.btnBackAndGo}
+        >
+          <ion-icon name="arrow-dropleft"></ion-icon>
+        </button>
       )}
       {buttons.map(btn => {
         if (btn === '...'){
           return (
-            <span key={btn}>{btn}</span>
+            <span key={btn} style={styles.dots}>{btn}</span>
           )
         } else {
           return (
@@ -30,6 +33,7 @@ const Pagination = ({
               key={btn}
               style={btn == currentPage ? styles.activeBtn : null}
               onClick={(e) => changePage(e.target.value)}
+              style={styles.btnDefault}
             >{btn}</button>
           )
         }
@@ -39,14 +43,44 @@ const Pagination = ({
           type='button'
           value='next'
           onClick={(e) => changePage(e.target.value)}
-        >Next</button>
+          style={styles.btnBackAndGo}
+        >
+          <ion-icon name="arrow-dropright"></ion-icon>
+        </button>
       )}
     </div>
   );
 };
 
 const styles = {
-  activeBtn: {backgroundColor: '#008489'},
+  activeBtn: {
+    backgroundColor: '#008489'
+  },
+  btnDefault: {
+    display: 'inline-block',
+    backgroundColor: 'transparent',
+    color: 'rgb(0, 132, 137)',
+    borderWidth: 'initial',
+    borderStyle: 'none',
+    borderColor: 'initial',
+    borderImage: 'initial',
+    textDecoration: 'none',
+    margin: '0 8px 0 8px',
+    fontFamily: 'Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif',
+    fontSize: '16px',
+    fontWeight: '200',
+  },
+  dots: {
+    verticalAlign: 'super'
+  },
+  btnBackAndGo: {
+    backgroundColor: 'transparent',
+    boxShadow: 'rgb(0, 132, 137) 0px 0px 0px 1px inset',
+    color: 'rgb(0, 132, 137)',
+    height: '32px',
+    width: '32px',
+    borderRadius: '50%',
+  }
 }
 
 export default Pagination;
