28a383e343556897938a0f0f30ce09cc7c8816aa
Elinex <eline.ax@gmail.com>
2019-07-13 22:55:14 -0700

    insert hr tag to review component

--- client/components/Review.js
+++ client/components/Review.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 
 const styles = {
   divMain: {
-    marginBottom: '48px'
+    // marginBottom: '48px'
   },
   user: {
     height: '48px',
@@ -48,7 +48,15 @@ const styles = {
     fontWeight: '200',
     lineHeight: '1.375em',
     color: '#484848',
-  }
+  },
+  hr: {
+    fontFamily: 'Circular, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, sans-serif',
+    fontSize: '14px',
+    lineHeight: '1px',
+    color: 'rgb(235, 235, 235)',
+    margin: '24px 0 24px 0',
+    opacity: '0.2'
+  },
 };
 
 const Review = ({review}) => {
@@ -56,27 +64,30 @@ const Review = ({review}) => {
   const text = review.text.charAt(0).toUpperCase() + review.text.slice(1);
 
   return (
-    <div style={styles.divMain}>
-      <div style={styles.user}>
-        <img
-          src={review.avatarUrl}
-          height="50"
-          width="50"
-          style={styles.img}
-        />
-        <div style={styles.userInfo}>
-          <div style={styles.userName}>
-            {review.username}
-          </div>
-          <div style={styles.createdAt}>
-            {createdAt}
+    <React.Fragment>
+      <div style={styles.divMain}>
+        <div style={styles.user}>
+          <img
+            src={review.avatarUrl}
+            height="50"
+            width="50"
+            style={styles.img}
+          />
+          <div style={styles.userInfo}>
+            <div style={styles.userName}>
+              {review.username}
+            </div>
+            <div style={styles.createdAt}>
+              {createdAt}
+            </div>
           </div>
         </div>
+        <div style={styles.divText}>
+          {text}
+        </div>
       </div>
-      <div style={styles.divText}>
-        {text}
-      </div>
-    </div>
+      <hr style={styles.hr}/>
+    </React.Fragment>
   );
 };
 
