078c2308b7cd1de3588a9a696bd1e9fd2565d94a
Elinex <eline.ax@gmail.com>
2019-07-15 11:45:13 -0700

    fix pagination css

--- client/components/Pagination.js
+++ client/components/Pagination.js
@@ -1,51 +1,7 @@
 import React from 'react';
-import pagination from '../helpers/pagination.js';
 
-const styles = {
-  divMain: {
-    display: 'flex',
-    alignItems: 'center'
-  },
-  activeBtn: {
-    display: 'inline-block',
-    width: '32px',
-    height: '32px',
-    backgroundColor: 'rgb(0, 132, 137)',
-    color: 'rgb(255, 255, 255)',
-    borderRadius: '16px',
-    margin: '0 8px 0 8px',
-    fontFamily: 'Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif',
-    fontSize: '16px',
-    fontWeight: '200',
-  },
-  btnDefault: {
-    display: 'inline-block',
-    width: '32px',
-    height: '32px',
-    backgroundColor: 'transparent',
-    color: 'rgb(0, 132, 137)',
-    borderStyle: 'none',
-    textDecoration: 'none',
-    margin: '0 8px 0 8px',
-    fontFamily: 'Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif',
-    fontSize: '16px',
-    fontWeight: '200',
-  },
-  btnBackAndGo: {
-    borderRadius: '50%',
-    backgroundColor: 'transparent',
-    color: 'rgb(0, 132, 137)',
-    boxShadow: 'rgb(0, 132, 137) 0px 0px 0px 1px inset',
-    color: 'rgb(0, 132, 137)',
-    height: '32px',
-    width: '32px',
-    textDecoration: 'none',
-    margin: '0 8px 0 8px',
-    fontFamily: 'Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif',
-    fontSize: '14px',
-    fontWeight: '200',
-  }
-};
+import pagination from '../helpers/pagination.js';
+import css from '../styles/pagination.css';
 
 // max 7 reviews per page
 const Pagination = ({
@@ -56,13 +12,13 @@ const Pagination = ({
   const buttons = pagination(currentPage, numBtns);
 
   return (
-    <div style={styles.divMain}>
+    <div className='main-pagination'>
       {currentPage !== 1 && (
         <button
           type='button'
           name='arrow-back'
           onClick={(e) => changePage(e.target.name)}
-          style={styles.btnBackAndGo}
+          className='btnBackAndGo'
         >
           <ion-icon name="arrow-back"></ion-icon>
         </button>
@@ -78,7 +34,7 @@ const Pagination = ({
               type='button'
               value={btn}
               key={btn}
-              style={btn === currentPage ? styles.activeBtn : styles.btnDefault}
+              className={btn === currentPage ? 'activeBtn' : 'btnDefault'}
               onClick={(e) => changePage(e.target.value)}
             >{btn}</button>
           );
@@ -88,7 +44,7 @@ const Pagination = ({
         <button
           type='button'
           onClick={(e) => changePage(e.target.name)}
-          style={styles.btnBackAndGo}
+          className='btnBackAndGo'
           name='arrow-forward'
         >
           <ion-icon name="arrow-forward"></ion-icon>
