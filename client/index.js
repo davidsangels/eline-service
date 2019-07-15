5b9966e4edb06bea312434c0909e529bb772cede
Elinex <eline.ax@gmail.com>
2019-07-15 11:21:16 -0700

    fix app css

--- client/index.js
+++ client/index.js
@@ -4,17 +4,9 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import css from './styles/index.css';
 
-// const styles = {
-//   divApp: {
-//     margin: '0 180px 0 180px',
-//     padding: '0 24px 0 24px'
-//   }
-// };
-
 ReactDOM.render(
   <div className='reviews-module'>
     <App/>
   </div>,
   document.getElementById('app')
 );
-
