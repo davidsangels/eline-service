8afa158960831a87a600b92144deb6dd6df9801a
Elinex <eline.ax@gmail.com>
2019-07-15 11:06:29 -0700

    fix index css

--- client/index.js
+++ client/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
+import css from './styles/index.css';
 
-const styles = {
-  divApp: {
-    margin: '0 180px 0 180px',
-    padding: '0 24px 0 24px'
-  }
-};
+// const styles = {
+//   divApp: {
+//     margin: '0 180px 0 180px',
+//     padding: '0 24px 0 24px'
+//   }
+// };
 
 ReactDOM.render(
-  <div style={styles.divApp}>
+  <div className='reviews-module'>
     <App/>
   </div>,
   document.getElementById('app')
